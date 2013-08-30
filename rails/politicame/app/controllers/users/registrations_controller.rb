201aa7dd0ac807a3327e5f57519aabf8ad15640c
Cassio Paixao <cassio.paixao@gmail.com>
2013-08-30 00:16:20 -0300

    Exception handling during user registration. Exceptions in this method were blocking user experience.

--- rails/politicame/app/controllers/users/registrations_controller.rb
+++ rails/politicame/app/controllers/users/registrations_controller.rb
@@ -7,4 +7,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
+
+  def create
+    begin
+      super
+    rescue Exception => ex
+      if ex.is_a? ActiveRecord::RecordNotUnique
+        flash[:error] = 'E-mail já cadastrado no sistema.'
+      else
+        flash[:error] = 'Desculpe, ocorreu um erro desconhecido ao tentar lhe registrar. Tente novamente mais tarde.'
+      end
+      redirect_to :action => 'new', :status => 303
+    end
+  end
 end
