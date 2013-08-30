8654f6dfa188daeeb1d2c38f9d8369335acbb62e
Cassio Paixao <cassio.paixao@gmail.com>
2013-08-29 23:47:57 -0300

    Prevents cache on registration views

--- rails/politicame/app/controllers/users/registrations_controller.rb
+++ rails/politicame/app/controllers/users/registrations_controller.rb
@@ -1,3 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
+  before_filter :set_cache
+  def set_cache
+    expires_now
+
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 end
